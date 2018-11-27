30db76fbaca393194d17e7ce62941afcc9c1431a
AbdelazeezAbd <38917371+AbdelazeezAbd@users.noreply.github.com>
2018-11-27 22:00:35 +0200

    Add files via upload

---
+++ positions.js
@@ -0,0 +1,33 @@
+// determaine universities locations
+var positions = [
+    {
+        name : 'Bahçeşehir University',
+        lat : 41.042165,
+        long : 29.009258
+    },
+    {
+        name : 'Istanbul Ünıversitesi',
+        lat : 41.012604,
+        long : 28.961836
+    },
+    {
+        name : 'Istanbul Aydın University',
+        lat : 40.991916,
+        long : 28.797530
+    },
+    {
+        name : 'Istanbul Technical University',
+        lat : 41.105593,
+        long : 29.025338
+    },
+    {
+        name : 'Sabancı University',
+        lat : 40.888534,
+        long : 29.373989
+    },
+    {
+        name : 'Yıldız Teknik University',
+        lat : 41.052087,
+        long : 29.010653
+    }
+];
